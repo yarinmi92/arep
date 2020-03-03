411fd8503ba3e78b3de45a8f3ade3b8dfce2236a
yarinmi92 <yarinmi92@gmail.com>
2020-03-03 20:59:25 +0200

    Updates

---
+++ precache-manifest.9c4525aa090515b6f215ca722514e9d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a227693e15142483c7a49b76367afc2d",
+    "url": "/arep/index.html"
+  },
+  {
+    "revision": "747603e7d2fe094c14b9",
+    "url": "/arep/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f7aead640c1eac8bbb75",
+    "url": "/arep/static/js/2.56bdfbf4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/arep/static/js/2.56bdfbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747603e7d2fe094c14b9",
+    "url": "/arep/static/js/main.b2bf18c9.chunk.js"
+  },
+  {
+    "revision": "e51315144e7679f01c1d",
+    "url": "/arep/static/js/runtime-main.7f7cbb95.js"
+  }
+]);
